472585be96fd529e4ec937b19994ceba1266a2cd
unknown <chankristineagcay@gmail.com>
2022-02-08 10:26:49 +0800

    Set everything but there are still errors to be fix

--- kchan_resume.py
+++ kchan_resume.py
@@ -1,11 +1,40 @@
-import json
+import os as access
 from fpdf import FPDF
 
-# format - PDF
-pdf = FPDF('P', 'cm', 'A4') 
-pdf.add_page()
 
-# Infos to be read
-resume_info= open('resume.json', 'r')
-res_maker= resume_info.read()
-raw_infos= json.loads(res_maker)
+ # Important Values
+User = 'KRISTINE AGCAY CHAN' 
+file_name = 'CHAN_Kristine.pdf' 
+char = 'Personal Details'
+json_file = "resume.json" 
+
+class PDF(FPDF):
+# setting the header
+    def header(self): 
+        self.image('header.png', 170, 0, 40, 0) 
+        self.image('me.png', 10, 0, 50, 0) 
+        self.set_font('helvetica', 'B', 25) 
+        self.cell(180, 35, User, ln = 1, align = 'R'); 
+        self.cell(90, 20, char) 
+        self.ln(10)
+
+# setting the data
+    def json_body(self, name):
+        with open(json_file) as fh: 
+            chr = fh.read() 
+        self.set_font("helvetica", "", 11)
+        self.multi_cell(0, 4, chr)
+
+# setting the page 
+    def print_chap(self, num, title, name): 
+        self.add_page()
+        self.json_body(name)
+
+
+# PDF maker
+Kchanpdf = PDF('P', 'mm', 'A4') 
+Kchanpdf.set_auto_page_break(margin = 0.5, auto = True) 
+Kchanpdf.print_chap(1, "", json_file)
+Kchanpdf.output(file_name)
+# Open the file 
+access.startfile(file_name)
